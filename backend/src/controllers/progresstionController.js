a84e22b2bd7f5e663d01e4ee89bfaade7314d21a
RoopakCS <roopaksenthilkumar@gmail.com>
2025-10-07 22:35:04 +0530

    add: implement update functionality for user progressions

--- backend/src/controllers/progresstionController.js
+++ backend/src/controllers/progresstionController.js
@@ -55,4 +55,37 @@ const deleteProgression = async (req, res) => {
 	}
 };
 
-module.exports = { createProgression, getUserProgression, deleteProgression };
+const updateProgression = async (req, res) => {
+	try {
+		const progression = await Progression.findById(req.params.id);
+
+		if (!progression) {
+			return res.status(404).json({ message: "Progression not found" });
+		}
+
+		if (progression.user.toString() !== req.user._id.toString()) {
+			return res.status(403).json({ message: "Not authorized" });
+		}
+
+		const { title, artistName, scaleKey, timeSignature, chords, tags } =
+			req.body;
+		progression.title = title || progression.title;
+		progression.artistName = artistName || progression.artistName;
+		progression.scaleKey = scaleKey || progression.scaleKey;
+		progression.timeSignature = timeSignature || progression.timeSignature;
+		progression.chords = chords || progression.chords;
+		progression.tags = tags || progression.tags;
+
+		await progression.save();
+		res.status(200).json(progression);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = {
+	createProgression,
+	getUserProgression,
+	deleteProgression,
+	updateProgression,
+};
--- backend/src/routes/progressionRoutes.js
+++ backend/src/routes/progressionRoutes.js
@@ -4,12 +4,14 @@ const {
 	createProgression,
 	getUserProgression,
 	deleteProgression,
+    updateProgression,
 } = require("../controllers/progresstionController");
 
 const progressionRouter = express.Router();
 
 progressionRouter.post("/", verifyAuth, createProgression);
 progressionRouter.get("/", verifyAuth, getUserProgression);
+progressionRouter.put("/:id", verifyAuth, updateProgression)
 progressionRouter.delete("/:id", verifyAuth, deleteProgression);
 
 module.exports = progressionRouter;
