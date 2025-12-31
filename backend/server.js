2773976ee9f66bfa666c26ee8dfa490381502f7b
RoopakCS <roopaksenthilkumar@gmail.com>
2025-12-31 16:01:20 +0530

    feat: configure CORS to allow requests from client URL

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const progressionRouter = require("./src/routes/progressionRoutes.js")
 require("dotenv").config();
 
 const app = express();
-app.use(cors())
+app.use(cors({
+	origin: [process.env.CLIENT_URL],
+	credentials: true
+}))
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
